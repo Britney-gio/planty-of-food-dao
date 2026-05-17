3f7117c843cf74ae48a58dac1636353cfcd1c378
Utente <giorgianieli@gmail.com>
2026-05-17 22:36:23 +0200

    Implementazione struttura Proposal e governance base

--- contracts/POFGovernanceDAO.sol
+++ contracts/POFGovernanceDAO.sol
@@ -12,9 +12,31 @@ contract POFGovernanceDAO is Ownable {
 
     uint256 public sharePrice;
     bool public isShareSaleActive;
+    uint256 public proposalCount;
 
     mapping(address => uint256) public shares;
     mapping(address => bool) public isMember;
+
+    enum VoteChoice { 
+        Against, 
+        For, 
+        Abstain
+    }
+
+    struct Proposal {
+        uint256 id;
+        string title;
+        string description;
+        uint256 deadline;
+        uint256 forVotes;
+        uint256 againstVotes;
+        uint256 abstainVotes;
+        bool approved;
+        bool executed;    
+    }
+
+    mapping(uint256 => Proposal) public ledgerProposals;
+    mapping(uint256 => mapping(address => bool)) public hasVoted;
 
     constructor (
         address _pofToken,
@@ -26,6 +48,11 @@ contract POFGovernanceDAO is Ownable {
         treasury = POFTreasury(_treasury);
         sharePrice = _sharePrice;
         isShareSaleActive = true;
+    }
+
+    modifier onlyMember(){
+        require(isMember[msg.sender], "Only DAO members can call this function");
+        _;
     }
 
     function buyShares(uint256 amount) external {
@@ -41,7 +68,22 @@ contract POFGovernanceDAO is Ownable {
         isShareSaleActive = false;
     }
 
+    function createProposal(
+        string calldata title,
+        string calldata description,
+        uint256 durationInDays
+    ) external onlyMember {
+        require(bytes(title).length > 0, "Title cannot be empty");
+        require(bytes(description).length > 0, "Description cannot be empty");
+        require(durationInDays > 0, "Duration must be greater than zero");
+        uint256 proposalId = proposalCount++;
+        Proposal storage newProposal = ledgerProposals[proposalId];
+        newProposal.id = proposalId;
+        newProposal.title = title;
+        newProposal.description = description;
+        newProposal.deadline = block.timestamp + (durationInDays * 1 days);
 
 
-
+    }
+    
 }
