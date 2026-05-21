dd4eaaee244e7ebdd55920fc8c7662531c154e47
Utente <giorgianieli@gmail.com>
2026-05-21 18:41:45 +0200

    Test acquisto shares, registraione membri e fondi arrivati alla treasury

--- test/POFGovernanceDAO.ts
+++ test/POFGovernanceDAO.ts
@@ -6,16 +6,19 @@ describe("POFGovernanceDAO", async function () {
 
     it("Should deploy POFToken, POFTreasury and POFGovernanceDAO", async function () {
         const { ethers } = await network.connect();
-        const [ owner] = await ethers.getSigners();    
+        const [ owner, Giorgia ] = await ethers.getSigners();    
+        // Deploy smart contract POFToken
         const POFToken = await ethers.getContractFactory("POFToken");
         const pofToken = await POFToken.deploy(
             ethers.parseEther("1000000")
         );
+        // Deploy smart contract POFTreasury
         const POFTreasury = await ethers.getContractFactory("POFTreasury");
         const treasury = await POFTreasury.deploy(
             await pofToken.getAddress(),
             owner.address
         );
+        // Deploy smart contract POFGovernanceDAO
         const POFGovernanceDAO = await ethers.getContractFactory("POFGovernanceDAO");
         const governanceDAO = await POFGovernanceDAO.deploy(
             await pofToken.getAddress(),
@@ -23,12 +26,31 @@ describe("POFGovernanceDAO", async function () {
             ethers.parseEther("10"),
             owner.address
         );
+        // Set governanceDAO address in treasury contract
         await treasury.setGovernanceDAO(await governanceDAO.getAddress());
+
         assert.ok(await pofToken.getAddress());
         assert.ok(await treasury.getAddress());
-        assert.ok(await governanceDAO.getAddress());
-        
-    
+        assert.ok(await governanceDAO.getAddress()); 
 
-})
+        await pofToken.transfer( 
+            Giorgia.address, 
+            ethers.parseEther("100")
+        );
+        await pofToken.connect(Giorgia).approve(
+            await governanceDAO.getAddress(), 
+            ethers.parseEther("100")
+        );
+        await governanceDAO.connect(Giorgia).buyShares(5);
+        const GiorgiaShares = await governanceDAO.shares(Giorgia.address);
+        assert.equal(GiorgiaShares, 5n);
+        const isGiorgiaMember = await governanceDAO.isMember(Giorgia.address);
+        assert.equal(isGiorgiaMember, true);
+        const treasuryBalance = await pofToken.balanceOf(await treasury.getAddress());
+        assert.equal(treasuryBalance, ethers.parseEther("50"));
+    })
+
+
+
+
 });
