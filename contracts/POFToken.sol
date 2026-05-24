ef28204f073afa7f923b17b351595efacc7adf6e
Utente <giorgianieli@gmail.com>
2026-05-24 12:10:27 +0200

    Sistemato POFToken.sol con controllo di sicurezza

--- contracts/POFToken.sol
+++ contracts/POFToken.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract POFToken is ERC20, Ownable {
     constructor(uint256 initialSupply)
         ERC20("Planty of Food Token", "POF")
         Ownable(msg.sender)
-        {
+    {
+        require(initialSupply > 0, "Initial supply must be greater than zero");
         _mint(msg.sender, initialSupply);
-        }
+    }
 }
