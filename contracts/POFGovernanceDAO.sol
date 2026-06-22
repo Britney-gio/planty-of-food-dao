887908a5608fb361748ba38aaee1d6c6b4776421
Giorgia Nieli <you@example.com>
2026-06-22 22:52:19 +0200

    aggiunto controllo su funzione delegateVote() per prevenire delega circolare

--- contracts/POFGovernanceDAO.sol
+++ contracts/POFGovernanceDAO.sol
@@ -173,6 +173,7 @@ contract POFGovernanceDAO is Ownable {
         require(memberDelegate != msg.sender, "Cannot delegate yourself");
         require(isMember[memberDelegate], "Delegate must be a DAO member");
         require(delegates[msg.sender] == address(0), "Vote already delegated");
+        require(delegates[memberDelegate] == address(0), "Delegate has already delegated");
         delegates[msg.sender] = memberDelegate;
         delegatedShares[memberDelegate] += shares[msg.sender];
     }
